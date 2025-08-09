jcsteh/reaKontrol

commit 938e6ddca9d3799d1a5fac82b63fea6721bd730c

Mk2/A/M: Tweak volume/pan knob scaling.

src/niMidi.cpp
@@ -295,16 +295,17 @@ class NiMidiSurface: public BaseSurface {
 		if (!track) {
 			return;
 		}
-		CSurf_SetSurfaceVolume(track, CSurf_OnVolumeChange(track, value * 0.1, true), nullptr);
+		CSurf_OnVolumeChange(track, value / 127.0, true);
 	}
 
 	void _onKnobPanChange(unsigned char command, signed char value) {
-		int numInBank = command - CMD_KNOB_VOLUME1;
+		int numInBank = command - CMD_KNOB_PAN1;
 		MediaTrack* track = CSurf_TrackFromID(numInBank + this->_bankStart, false);
 		if (!track) {
 			return;
 		}
-		CSurf_SetSurfacePan(track, CSurf_OnPanChange(track, value * 1.0, true), nullptr);
+		const double PAN_SCALE_FACTOR = 127 * 8;
+		CSurf_OnPanChange(track, value / PAN_SCALE_FACTOR, true);
 	}
 
 	void _sendCc(unsigned char command, unsigned char value) {

commit ba9e1e99e62b367d149569ec5c9e38e0d7527105

When adjusting track volume/pan from Kontrol, the display is now updated correctly.
Even though CSurf_OnVolumeChange seems to adjust the track volume in REAPER, it seems we need to call CSurf_SetSurfaceVolume as well.
Otherwise, surfaces (including Kontrol) don't get notified about the change.
The same is true for the pan functions.
We originally did this, but this was regressed in 938e6ddc.

src/niMidi.cpp
@@ -376,7 +376,7 @@ class NiMidiSurface: public BaseSurface {
 		if (!track) {
 			return;
 		}
-		CSurf_OnVolumeChange(track, value / 127.0, true);
+		CSurf_SetSurfaceVolume(track, CSurf_OnVolumeChange(track, value / 127.0, true), nullptr);
 	}
 
 	void _onKnobPanChange(unsigned char command, signed char value) {
@@ -386,7 +386,7 @@ class NiMidiSurface: public BaseSurface {
 			return;
 		}
 		const double PAN_SCALE_FACTOR = 127 * 8;
-		CSurf_OnPanChange(track, value / PAN_SCALE_FACTOR, true);
+		CSurf_SetSurfacePan(track, CSurf_OnPanChange(track, value / PAN_SCALE_FACTOR, true), nullptr);
 	}
 
 	void _sendCc(unsigned char command, unsigned char value) {